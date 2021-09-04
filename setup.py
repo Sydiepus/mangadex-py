b2f373a8093f0a2887f8f0778dd9c2bcdd52922f
None Noen <sydiepus@gmail.com>
2021-09-04 22:11:15 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Environment :: Console",
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
